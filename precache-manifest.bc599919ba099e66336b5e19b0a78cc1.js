34f5fb892be036e46926ccbe5a4d0f178c4ab82a
kresna <fristaf.kresna@gmail.com>
2020-07-24 08:58:03 +0700

    Updates

---
+++ precache-manifest.bc599919ba099e66336b5e19b0a78cc1.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b14b484d689ccfdf1a5a2dc23bed69b",
+    "url": "/proto01/index.html"
+  },
+  {
+    "revision": "42f3fc746bee015be767",
+    "url": "/proto01/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6ea4916adbbd77a469b7",
+    "url": "/proto01/static/css/main.adc852b8.chunk.css"
+  },
+  {
+    "revision": "42f3fc746bee015be767",
+    "url": "/proto01/static/js/2.0016957f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/proto01/static/js/2.0016957f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ea4916adbbd77a469b7",
+    "url": "/proto01/static/js/main.7dfd71b9.chunk.js"
+  },
+  {
+    "revision": "b527257a0850da232d5f",
+    "url": "/proto01/static/js/runtime-main.86e3feb2.js"
+  },
+  {
+    "revision": "5c570427ee23f69853d28aec805eee79",
+    "url": "/proto01/static/media/instagram.5c570427.png"
+  },
+  {
+    "revision": "ffda0c9aaad2af29f2209ef372afa508",
+    "url": "/proto01/static/media/logoNannys.ffda0c9a.png"
+  },
+  {
+    "revision": "271c5e79625a18f98bbd1ee58bfd5575",
+    "url": "/proto01/static/media/low.271c5e79.png"
+  },
+  {
+    "revision": "3331eda294c3fd832a92b33e426c8450",
+    "url": "/proto01/static/media/package.3331eda2.png"
+  },
+  {
+    "revision": "04c269476adda5e1dcae57c8198f1eaf",
+    "url": "/proto01/static/media/product (1).04c26947.jpg"
+  },
+  {
+    "revision": "a2ea8677a4a2fdbdd1df5833db18d974",
+    "url": "/proto01/static/media/product (2).a2ea8677.jpg"
+  },
+  {
+    "revision": "6923b9293ead47d9f2bdbc4684a09d50",
+    "url": "/proto01/static/media/product (3).6923b929.jpg"
+  },
+  {
+    "revision": "9f0f638924b37b310f6ab45e9a2abfba",
+    "url": "/proto01/static/media/sampel_gmap.9f0f6389.jpg"
+  },
+  {
+    "revision": "fb87c131881a78314baf2fb8bef1844e",
+    "url": "/proto01/static/media/scenery1.fb87c131.jpg"
+  },
+  {
+    "revision": "ea3424e4174fdad9dd7c711cf95802c6",
+    "url": "/proto01/static/media/scenery2.ea3424e4.jpg"
+  },
+  {
+    "revision": "b482aed22f75413390a3a76afa2e34f6",
+    "url": "/proto01/static/media/scenery3.b482aed2.jpg"
+  },
+  {
+    "revision": "3f28ba3d895d3f190c7ef9ed9fa61916",
+    "url": "/proto01/static/media/trust.3f28ba3d.png"
+  }
+]);
